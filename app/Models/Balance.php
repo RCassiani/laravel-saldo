5518823fa1a2974df1237b74383e6ba4fa5eae07
Rodrigo Cassiani <rfratta@artit.com.br>
2018-11-05 09:56:32 -0200

    Valor de Saldo Real adicionado

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Balance extends Model
 {
     public $timestamps = false;
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
 }
